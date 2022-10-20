ef966990a9bfd9baec2587dcb26c4da9e70094ed
sqais <sqais379@mtroyal.ca>
2022-10-19 20:51:28 -0600

    Header Footer Single Page Check

--- tho1341/single-page.php
+++ tho1341/single-page.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>COMP 3512 Assign1</title>
+    <h3>Tim Ho & Shahmir Qaiser</h3>
 </head>
 <body>
 
 
-    
+    <footer>
+        <p>
+            COMP 3512 Web 2 | 
+            <a href="https://github.com/tho1341">Timothy Ho</a> | 
+            <a href="https://github.com/sqais">Shahmir Qaiser</a> | 
+            <a href="https://github.com/tho1341/asg1.git">GitHub Repo</a>
+        </p>
+    </footer>
 </body>
 </html>
