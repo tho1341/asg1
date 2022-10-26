d81d11dd2df366ab787943581274c969f5f41a80
tho1341 <77995422+tho1341@users.noreply.github.com>
2022-10-25 23:58:41 -0600

    browse works 100%

--- tho1341/browse-results-helper.php
+++ tho1341/browse-results-helper.php
@@ -3,7 +3,10 @@
 class listOutput{
     function listAll($music){
         foreach($music as $row){
-            echo $row['title'] . " | " . $row['artist_name'] . " | "  . $row['year'] . " | " . $row['genre_name'] . " | " . $row['popularity'] .  "<br>";
+            echo $row['title'] . " | " . $row['artist_name'] . " | "  . $row['year'] . " | " . $row['genre_name'] . " | " . $row['popularity'];
+            echo "<form><input type='submit' value='addFav' name='addFav'>";
+            echo "<input type='submit' value='view' name='view'></form>";
+            echo "<br>";
         }
     }
 }
--- tho1341/browse-results.php
+++ tho1341/browse-results.php
@@ -9,23 +9,6 @@ try{
 $conn = DBHelper::createConnection(array(DBCONNSTRING,DBUSER,DBPASS));
 
 $musicGateway = new MusicDB($conn);
-//$music = $musicGateway->getAll();
-
-//conditional to see if came from search page
-if(isset($_POST['submit'])){
-    echo 'ehehehehhe';
-    if(empty($_POST['title']) ){
-        echo 'teststestestst';
-    }
-}
-
-$array = ['title', 'artists', 'genre', 'year', 'popularity'];
-
-//foreach($array as $row){
-//    if (empty($_POST[$row])) {
-//        echo "Email is required";
-//}
-//}
 
 //if(isset($_POST['artists']) && !empty($_POST['artists'])){
 if(isset($_POST['artists']) && $_POST['artists']!=0){
@@ -33,19 +16,9 @@ if(isset($_POST['artists']) && $_POST['artists']!=0){
 
 } else if(isset($_POST['genre']) && $_POST['genre']!=0){
     $music = $musicGateway->getSongGenre($_POST['genre']);
-} else if(isset($_POST['title']) && $_POST['title']!=""){
-    
-    $music = $musicGateway->getSongTitle($_POST['title']);
 
-//     $sql = "SELECT title, duration, year, genres.genre_name, artists.artist_name, types.type_name, bpm, energy, danceability, liveness, valence, acousticness, speechiness, popularity
-//     FROM songs 
-//     LEFT JOIN genres ON genres.genre_id = songs.genre_id
-//     LEFT JOIN artists ON artists.artist_id = songs.artist_id
-//     LEFT JOIN types ON artists.artist_type_id = types.type_id 
-//     WHERE title LIKE :search";
-// $statement = $conn->prepare($sql);
-// $statement->bindValue(":search", '%' . $_POST['title'] . '%');
-// $statement->execute();
+} else if(isset($_POST['title']) && $_POST['title']!=""){
+    $music = $musicGateway->getSongTitle($_POST['title']);
 
 } else if(isset($_POST['year']) && $_POST['year']=='less'){
     $music = $musicGateway->getSongYear($_POST['lessText'], null);
@@ -61,19 +34,12 @@ if(isset($_POST['artists']) && $_POST['artists']!=0){
 }
 else if(isset($_POST['showAll'])){
     $music = $musicGateway->getAll();
+} 
+else {
+    $music = 0;
 }
 
-
-
-
-
-
 } catch (Exception $e) { die( $e->getMessage() ); }
-
-
-
-
-
 
 ?>
 
@@ -89,31 +55,22 @@ else if(isset($_POST['showAll'])){
 <body>
     <?php
     
-
-
-    $count=1;
-    while($count<5){
-        foreach($array as $row){
-            if (!empty($_POST[$row])) {
-                foreach($music as $row){
-                    echo $row['title'] . " | " . $row['artist_name'] . " | "  . $row['year'] . " | " . $row['genre_name'] . " | " . $row['popularity'] .  "<br>";
-                }
-            }
+        if (!($_SERVER['REQUEST_METHOD'] == 'POST')){
+            $output = new listOutput();
+            $music = $musicGateway->getAll();
+            $output->listAll($music);
         }
-        $count++;
-    }
-        if($count=5 && !isset($_POST['showAll'])){
+        else if($music == 0){
             echo "No input. Please Enter Search Field";
         }
         else{
             $output = new listOutput();
             $output->listAll($music);
-            //$music = $musicGateway->getAll();
         }
     ?>
 
     <form action="browse-results.php" method="post">
-    <input type="submit" value="Search" name='showAll'>
+    <input type="submit" value="Show All" name='showAll'>
     </form>
 
     </body>
