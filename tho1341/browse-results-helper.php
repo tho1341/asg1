8a07d12b5808edac798fe741d0b2395051f7e63f
tho1341 <77995422+tho1341@users.noreply.github.com>
2022-10-26 19:12:10 -0600

    fixed title link

--- tho1341/browse-results-helper.php
+++ tho1341/browse-results-helper.php
@@ -4,7 +4,7 @@ class listOutput{
     function listAll($music){
         foreach($music as $row){
             echo '<tr>';
-            echo '<td><a href="single-page.php?song_id="' . $row['song_id'] . '">' . $row['title'] . '</a>';
+            echo '<td>'. '<a href="single-page.php?song_id='. $row['song_id'] . '">' . $row['title'] . '</a></td>'; 
                 echo '<td>'. $row['artist_name'] .'</td>'; 
                 echo '<td>'. $row['year'] .'</td>'; 
                 echo '<td>'. $row['genre_name'] .'</td>'; 
