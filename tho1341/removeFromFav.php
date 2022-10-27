1bb7bc05692d921256194cb8272d06d2926e9e47
sqais <sqais379@mtroyal.ca>
2022-10-26 22:48:01 -0600

    remove

---
+++ tho1341/removeFromFav.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'includes/config.inc.php';
+
+//sesssion start
+session_start();
+
+//check if exists 
+if(!isset($_SESSION["Favorites"])){
+    //init empty array 
+    $_SESSION["Favorites"] = [];
+}
+
+//get fav array for the current session
+$favorites = $_SESSION["Favorites"];
+
+if($key = array_search($_GET["song_id"], $favorites)){
+    unset($favorites[$key]);
+}
+
+
+//if(array_search($_GET["song_id"], $favorites) == false){
+  //  $favorites[] = $_GET["song_id"];
+//}
+
+//resave array for user session
+$_SESSION["Favorites"] = $favorites;
+
+//redirect to view-fav page
+header("Location: view-fav.php");
+?>
