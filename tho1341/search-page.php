2e0e05f1ff61cf39ecf4cb41044ebfe5c25286f0
sqais <sqais379@mtroyal.ca>
2022-10-27 00:05:36 -0600

    css

--- tho1341/search-page.php
+++ tho1341/search-page.php
@@ -1,22 +1,13 @@
 <?php
 require_once 'includes/config.inc.php';
 require_once 'includes/db-classes.inc.php';
-
-
 try{
-
 $conn = DBHelper::createConnection(array(DBCONNSTRING,DBUSER,DBPASS));
-
 $musicGateway = new MusicDB($conn);
 $music = $musicGateway->getAll();
-
 $artists = $musicGateway->getAllArtist();
-
 $genre = $musicGateway->getAllGenre();
-
-
 } catch (Exception $e) { die( $e->getMessage() ); }
-
 ?>
 
 <!DOCTYPE html>
@@ -25,63 +16,52 @@ $genre = $musicGateway->getAllGenre();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheet.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>search-page</title>
 </head>
 <body>
-    
 <div class="searchForm">
         <form action="browse-results.php" method="post">
             <label>Title:</label>
-            <input type="text" name="title" size=30/>
-            
+            <input class="enterText" type="text" name="title" size=20/>
             <label>Artist:</label>
-            <select name="artists">
+            <select class="dropDown" name="artists">
             <option value='0'></option> 
                 <?php
                     foreach($artists as $row){
                         echo "<option value='" . $row['artist_name'] . "'>" . $row['artist_name'] . "</option>";
                     }
-
                 ?>
             </select>
-            
+            <br>
             <label>Genre:</label>
-            <select name="genre">
+            <select class="dropDown" name="genre">
             <option value='0'></option>
             <?php
                     foreach($genre as $row){
                         echo "<option value='" . $row['genre_name'] . "'>" . $row['genre_name'] . "</option>";
                     }
-
                 ?>
             </select>     
             <br>
-
             <label>Year</label>
             <br>
             <label><input type="radio" name="year" value="less">Less: </label>
-                <input type="text" name="lessText" size=30/>
+                <input class="enterText" type="text" name="lessText" size=10/>
             <br>
             <label><input type="radio" name="year" value="great">Greater: </label>
-                <input type="text" name="greatText" size=30/>
-            
+                <input class="enterText" type="text" name="greatText" size=10/>
             <br>
-            <br>
-
             <label>Popularity</label>
             <br>
             <label><input type="radio" name="popularity" value="less">Less: </label>
-                <input type="text" name="popLess" size=50/>
+                <input class="enterText" type="text" name="popLess" size=10/>
             <br>
             <label><input type="radio" name="popularity" value="great">Greater: </label>
-                <input type="text" name="popGreat" size=50/>
-
+                <input class="enterText" type="text" name="popGreat" size=10/>
             <br>
-
             <input type="submit" value="Search">
         </form>
 </div>
-
 </body>
 </html>
