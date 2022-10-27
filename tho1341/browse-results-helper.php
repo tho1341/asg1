23aab2470cd1c3ffea845c51270b4f0a8bbef18f
tho1341 <77995422+tho1341@users.noreply.github.com>
2022-10-26 22:48:28 -0600

    remove view

--- tho1341/browse-results-helper.php
+++ tho1341/browse-results-helper.php
@@ -9,7 +9,22 @@ class listOutput{
                 echo '<td>'. $row['year'] .'</td>'; 
                 echo '<td>'. $row['genre_name'] .'</td>'; 
                 echo '<td>'. $row['popularity'] .'</td>'; 
-            echo '<td>'. '<a href="view-fav.php?song_id='. $row['song_id'] . '">Add to Favourites' . '</a></td>'; 
+            echo '<td>'. '<a href="addToFav.php?song_id='. $row['song_id'] . '">Add to Favourites' . '</a></td>'; 
+            echo '<td>'. '<a href="single-page.php?song_id='. $row['song_id'] . '">View' . '</a></td>'; 
+            echo "<tr>";
+            echo "<br>";
+        }
+    }
+
+    function listAllRemove($music){
+        foreach($music as $row){
+            echo '<tr>';
+            echo '<td>'. '<a href="single-page.php?song_id='. $row['song_id'] . '">' . $row['title'] . '</a></td>'; 
+                echo '<td>'. $row['artist_name'] .'</td>'; 
+                echo '<td>'. $row['year'] .'</td>'; 
+                echo '<td>'. $row['genre_name'] .'</td>'; 
+                echo '<td>'. $row['popularity'] .'</td>'; 
+            echo '<td>'. '<a href="addToFav.php?song_id='. $row['song_id'] . '">Remove' . '</a></td>'; 
             echo '<td>'. '<a href="single-page.php?song_id='. $row['song_id'] . '">View' . '</a></td>'; 
             echo "<tr>";
             echo "<br>";
--- tho1341/view-fav.php
+++ tho1341/view-fav.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'includes/config.inc.php';
 require_once 'includes/db-classes.inc.php';
+require_once 'browse-results-helper.php';
 
 session_start();
 //initializing empty array
-if(!isset($_SESSION["Favorites"])){
-    $_SESSION["Favorites"] =[];
-}
+// if(!isset($_SESSION["Favorites"])){
+//     $_SESSION["Favorites"] =[];
+// }
 //retrieve array for this session
 $favorites = $_SESSION["Favorites"];
 
@@ -36,22 +37,35 @@ $musicGateway = new MusicDB($conn);
                 //$outputFavSongs = $musicGateway -> outputFavSong($favSongs);
                 foreach($favSongs as $row){
                     echo $row["title"];
-                    echo $_GET["song_id"];
+                    //echo $_GET["song_id"];
                 }
-                
-                //print_r($favSongs);
+
             }
         //}
-    ?>
-<!--
-                <tr>
-                    <td><?=$songs["title"]?></td>
-                    <td><?=$artist["artist"]?></td>
+    
+try{
+
+$conn = DBHelper::createConnection(array(DBCONNSTRING,DBUSER,DBPASS));
+
+$musicGateway = new MusicDB($conn);
+
+//if(isset($_POST['artists']) && !empty($_POST['artists'])){
 
 
-                </tr>
+} catch (Exception $e) { die( $e->getMessage() ); }
+   
+        if (!($_SERVER['REQUEST_METHOD'] == 'POST')){
+            $output = new listOutput();
+            $music = $musicGateway->GetByIDFav();
+            $output->listAllRemove($music);
+        }
         
- -->
+?>
+
+    <form action="browse-results.php" method="post">
+    <input type="submit" value="Show All" name='showAll'>
+    </form>
+
         <br>
         <a href="emptyFavorites.php">Empty Favorites</a>
         <br>
